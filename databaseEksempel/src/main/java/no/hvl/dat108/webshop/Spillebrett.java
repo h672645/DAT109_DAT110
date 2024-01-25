9180f60c504d8d398c494d7542477d557afa8e65
Ibrahim Elrahmoun <672644@stud.hvl.no>
2024-01-25 18:16:55 +0100

    endring i klasse spillebrett

--- databaseEksempel/src/main/java/no/hvl/dat108/webshop/Spillebrett.java
+++ databaseEksempel/src/main/java/no/hvl/dat108/webshop/Spillebrett.java
@@ -1,13 +1,19 @@
 package no.hvl.dat108.webshop;
-
+/**Brett klasse */
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Random;
+
+
 
 public class Spillebrett {
 
-	private List<Rute> ruteliste;
+	private List<Rute> ruteliste; // her oppretter man en liste av type 'Rute'
+	
+
+	//konstruktør som vil opprette ny objekt 'brett'.
+	public Spillebrett(){
+		ruteliste= new ArrayList<Rute>();
+	}
 	
 	
 	
