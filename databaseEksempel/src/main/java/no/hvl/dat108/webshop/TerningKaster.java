2defeb74f4d4436a59f2727efbecf90b6698316d
L672652 <672652@stud.hvl.no>
2024-01-25 14:24:38 +0100

    Test added

--- databaseEksempel/src/main/java/no/hvl/dat108/webshop/TerningKaster.java
+++ databaseEksempel/src/main/java/no/hvl/dat108/webshop/TerningKaster.java
@@ -1,5 +1,15 @@
 package no.hvl.dat108.webshop;
 
-public class TerningKaster {
+import java.util.Random;
 
+public class TerningKaster {
+	private int terningkast;
+	public TerningKaster() {
+		this.terningkast=0;
+	}
+	public int kastTerning(){
+		Random random = new Random();
+		this.terningkast=1 + random.nextInt(5);
+		return this.terningkast;
+	}
 }
