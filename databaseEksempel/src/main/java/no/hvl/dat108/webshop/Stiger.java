56c796bde0049f79eef65ef2acacd079937ef33c
h672645 <672645@stud.hvl.no>
2024-02-01 12:53:16 +0100

    Revert "Merge branch 'main' of https://github.com/h672645/DAT109_DAT110"
    This reverts commit 10d598c1cb3e0373a9eefaa53760ea996018ee9f, reversing
    changes made to 724346fe4e68cc98b4c437b88e715eeab162a5f6.

---
+++ databaseEksempel/src/main/java/no/hvl/dat108/webshop/Stiger.java
@@ -0,0 +1,20 @@
+package no.hvl.dat108.webshop;
+
+public class Stiger {
+    private int lengde;
+    //oppretter objekt 
+    public Stiger(){
+
+        this.lengde=0;
+    }
+    // getter
+    public int getLengde(){
+        return lengde;
+    }
+    //setter
+    public void setLengde(int lengde){
+        this.lengde=lengde;
+    }
+
+
+}
