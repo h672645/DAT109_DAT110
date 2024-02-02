08b2f0b00edbf21296570c23749e244e6e3dfec1
L672652 <672652@stud.hvl.no>
2024-02-02 11:13:21 +0100

    message server tror jeg er good

--- dat110-project1-main/dat110-project1-main/src/main/java/no/hvl/dat110/messaging/MessagingServer.java
+++ dat110-project1-main/dat110-project1-main/src/main/java/no/hvl/dat110/messaging/MessagingServer.java
@@ -2,6 +2,7 @@ package no.hvl.dat110.messaging;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import no.hvl.dat110.TODO;
 
@@ -28,13 +29,19 @@ public class MessagingServer {
 
 		MessageConnection connection = null;
 		
-		connection = new MessageConnection(this.welcomeSocket);
+		
+		Socket socket;
+		try {
+			socket = this.welcomeSocket.accept();
+			connection = new MessageConnection(socket);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		// TODO - START
 		// accept TCP connection on welcome socket and create messaging connection to be returned
 
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
 		
 		// TODO - END
 		
