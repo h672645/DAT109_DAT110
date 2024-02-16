ed6d7592f413834701a7e7a486d7634b4ae83251
IbrahimRH <672644@stud.hvl.no>
2024-02-16 16:51:08 +0100

    d
    dd

--- dat109_oblig2/src/main/java/no/hvl/dat109/Utleiekontor.java
+++ dat109_oblig2/src/main/java/no/hvl/dat109/Utleiekontor.java
@@ -1,5 +1,36 @@
 package no.hvl.dat109;
 
-public class Utleiekontor {
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utleiekontor {
+    private String navn;
+    private String sted;
+    private List<Bil> tilgjengeligeBiler;
+
+    public Utleiekontor(String navn, String sted) {
+        this.navn = navn;
+        this.sted = sted;
+        this.tilgjengeligeBiler = new ArrayList<>();
+    }
+
+
+    
+    // Legg til en bil i utleiekontoret
+    public void leggTilBil(Bil bil) {
+        tilgjengeligeBiler.add(bil);
+    }
+
+    // Fjern en bil fra utleiekontoret
+    public void fjernBil(Bil bil) {
+        tilgjengeligeBiler.remove(bil);
+    }
+
+    // Hent alle tilgjengelige biler i utleiekontoret
+    public List<Bil> hentTilgjengeligeBiler() {
+        return tilgjengeligeBiler;
+    }
+
+    // Annen funksjonalitet og metoder etter behov
 }
